1ced022bd2fb46a5e99a4550f406973ebd036aac
Benedictoz <benedict.glico@gmail.com>
2022-12-06 23:17:27 +0800

    Creating New Password and Password Confirm

---
+++ NewPass.js
@@ -0,0 +1,54 @@
+	//Validtion Code For Inputs
+
+    const form = document.querySelector("form"),
+    passField = form.querySelector(".new-password"),
+    passInput = passField.querySelector(".password"),
+    cPassField = form.querySelector(".confirm-password"),
+    cPassInput = cPassField.querySelector(".cPassword");
+  const eyeIcons = document.querySelectorAll(".show-hide");
+  
+  eyeIcons.forEach((eyeIcon) => {
+    eyeIcon.addEventListener("click", () => {
+      const pInput = eyeIcon.parentElement.querySelector("input");
+      if (pInput.type === "password") {
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return (pInput.type = "text");
+      }
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+      pInput.type = "password";
+    });
+  });
+  
+  function createPass() {
+    const passPattern =
+      /^[A-Za-z\d@$!%*?&]{8,}$/;
+  
+    if (!passInput.value.match(passPattern)) {
+      return passField.classList.add("invalid"); 
+    }
+    passField.classList.remove("invalid"); 
+  }
+  
+  function confirmPass() {
+    if (passInput.value !== cPassInput.value || cPassInput.value === "") {
+      return cPassField.classList.add("invalid");
+    }
+    cPassField.classList.remove("invalid");
+  }
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createPass();
+    confirmPass();
+  
+    passInput.addEventListener("keyup", createPass);
+    cPassInput.addEventListener("keyup", confirmPass);
+  
+    if (
+      !passField.classList.contains("invalid") &&
+      !cPassField.classList.contains("invalid")
+    ) {
+      location.href = form.getAttribute("action");
+    }
+  });
+ 
